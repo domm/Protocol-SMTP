5e6b5e52c6e59e091733a7047d138e2b076a9f91
Tom Molesworth <tom@perlsite.co.uk>
2014-02-27 00:45:11 +0000

    Import github version
    git-svn-id: http://entitymodel.com/svn/trunk/Protocol-SMTP@2273 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/Protocol/SMTP.pm
+++ lib/Protocol/SMTP.pm
@@ -1,13 +1,59 @@
 package Protocol::SMTP;
-# ABSTRACT: Do stuff
+# ABSTRACT: Mail sending protocol implementation
 use strict;
 use warnings;
-use 5.10.0;
 
-our $VERSION = '1.000';
+our $VERSION = '0.001';
 
-=pod
+=head1 NAME
+
+Protocol::SMTP - abstract support for the SMTP mail sending protocol
+
+=head1 SYNOPSIS
+
+ use Protocol::SMTP::Client;
+
+=head1 DESCRIPTION
+
+Features supported at the moment:
+
+=over 4
+
+=item * STARTTLS upgrading
+
+=item * Multiple recipients per outgoing email
+
+=item * 8BITMIME body encoding
+
+=item * SASL authentication
+
+=back
+
+Missing features:
+
+=over 4
+
+=item * Everything else
+
+=back
 
 =cut
 
 1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
+=head1 AUTHOR
+
+Tom Molesworth <cpan@entitymodel.com>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2012-2014. Licensed under the same terms as Perl itself.
+
